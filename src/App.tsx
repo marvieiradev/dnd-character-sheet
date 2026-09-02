766787e110e5bf9ebe0f43d158bab67332435720
Marcos Vieira <marcosvieira482@gmail.com>
2026-09-01 22:56:12 -0300

    feat: added initial code for the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-function App() {
+import { Toaster } from "sonner";
+import Home from "./pages/Home";
 
+function App() {
   return (
     <>
-      
+      <Toaster theme="dark" position="bottom-right" />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
